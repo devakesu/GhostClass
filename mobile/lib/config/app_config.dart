ea9d8d7f70664cf216b0a5cc6ab1191196a4f441
Devanarayanan <fusion@devakesu.com>
2026-09-15 21:10:22 +0000

    chore: bump application version to 4.6.5

--- mobile/lib/config/app_config.dart
+++ mobile/lib/config/app_config.dart
@@ -89,7 +89,7 @@ class AppConfig {
 
   /// Current application version
   static String get appVersion =>
-      const String.fromEnvironment('APP_VERSION', defaultValue: '4.6.2');
+      const String.fromEnvironment('APP_VERSION', defaultValue: '4.6.5');
 
   /// Commit SHA injected by CI for release builds.
   static String get appCommitSha =>
